7a4dae35c2f9ba35105165aa957dfdaf96252e3d
minindu-alwis <minindualwis@gmail.com>
2025-01-01 10:32:15 +0530

    2025 first commite

---
+++ CustomerManagmentCRUD/src/main/java/Starter.java
@@ -0,0 +1,25 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/CustomerManageMainForm.fxml"))));
+            stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+
+        launch();
+
+
+    }
+
+
+
+}
