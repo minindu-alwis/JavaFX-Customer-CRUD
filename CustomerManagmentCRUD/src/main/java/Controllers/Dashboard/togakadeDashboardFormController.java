70915d304ad938f03b795636ccc94b4c22f7c843
minindu-alwis <minindualwis@gmail.com>
2025-01-08 09:33:34 +0530

    ADD THE ITEM CRUDdd

--- CustomerManagmentCRUD/src/main/java/Controllers/Dashboard/togakadeDashboardFormController.java
+++ CustomerManagmentCRUD/src/main/java/Controllers/Dashboard/togakadeDashboardFormController.java
@@ -1,4 +1,37 @@
 package Controllers.Dashboard;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class togakadeDashboardFormController {
+    public AnchorPane dashboardAncorPane;
+
+    public void btnCustomerOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/CustomerManageMainForm.fxml"));
+            AnchorPane pane = loader.load();
+            dashboardAncorPane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to load the Customer Manage Main Form: " + e.getMessage()).show();
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public void btnItemFormOnAction(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/ItemForm.fxml"));
+            AnchorPane pane = loader.load();
+            dashboardAncorPane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to load the Customer Manage Main Form: " + e.getMessage()).show();
+            e.printStackTrace();
+        }
+    }
 }
--- CustomerManagmentCRUD/src/main/java/Starter.java
+++ CustomerManagmentCRUD/src/main/java/Starter.java
@@ -7,7 +7,7 @@ public class Starter extends Application {
     @Override
     public void start(Stage stage) throws Exception {
 
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/CustomerManageMainForm.fxml"))));
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/thogakadeDashboard.fxml"))));
             stage.show();
 
 
